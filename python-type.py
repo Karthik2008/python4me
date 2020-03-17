1da1b293f2cd67e8d1423d94d308d1adb43e42e9
Karthik <bkp.karthi@gmail.com>
2020-03-17 22:56:25 +0530

    Type ( ) method

---
+++ python-type.py
@@ -0,0 +1,12 @@
+# The below example shows you "How To Define A type And Print Its Type."
+# Let us assign an integer and a string with number 1 and define the type.
+num1 = 1
+num2 = "1"
+print (num1)
+print (num2)
+
+# The output of above code prints 1 and 1 which looks similar but one is integer and one is string.
+# To find the type we use the following method: type()
+
+print(type(num1))
+print(type(num2)) .
